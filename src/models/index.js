6e75299c398657096225d77e69bcad7993996a60
aunn <aunn_it08@hotmail.com>
2017-09-19 23:23:44 +0700

    update model

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const path = require('path')
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOSTNAME,
