b23521758858ae8a504f9af165a768216fea856f
aunn <aunn_it08@hotmail.com>
2017-09-20 21:19:21 +0700

    add mysql hello world

--- src/server.js
+++ src/server.js
@@ -2,10 +2,17 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const graphqlHTTP = require('koa-graphql')
 
+const db = require('./models')
+
 const app = new Koa()
 const router = new Router()
 
 const graphqlSchema = require('./schema')
+
+router.get('/test', async function (ctx) {
+  const result = await db.sequelize.query('SELECT "Hello World!"', { type: db.sequelize.QueryTypes.SELECT})
+  ctx.body = result
+})
 
 router.all('/graphql', graphqlHTTP({
   schema: graphqlSchema,
