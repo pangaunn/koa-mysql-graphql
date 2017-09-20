d527826e5cec80290b2aa9bca8ece3d3f967a64f
aunn <aunn_it08@hotmail.com>
2017-09-20 20:53:59 +0700

    add sequilze connection

--- src/models/index.js
+++ src/models/index.js
@@ -6,3 +6,19 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   dialect: 'mysql'
 })
 
+let db = fs.readdirSync(__dirname)
+  .filter(f => (f.indexOf('.') !== 0) && (f !== 'index.js'))
+  .reduce((obj, f) => {
+    let model = sequelize.import(path.join(__dirname, f))
+    obj[model.name] = model
+    return obj
+  }, {})
+
+Object.keys(db).forEach(model => {
+  db[model].associate(db)
+})
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
